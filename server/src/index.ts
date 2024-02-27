27cd353c961ad7ef3525f394d5aeec10a20a018f
Tom Scerri <hello@deeplift.dev>
2024-02-27 21:07:08 +1100

    Update package.json and server index.ts, add logger and cors handler for hono.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,13 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import { logger } from 'hono/logger'
 
-const app = new Hono()
+const app = new Hono();
+app.use('/*', cors());
+app.use(logger());
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('Hello Hono!');
 })
 
 export default app
